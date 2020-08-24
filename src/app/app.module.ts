47a2cc2349ae1dfbd6a0cf7094f2b39fb7f0df62
Tacho Zhelev <tacho.zhelev@paysafe.com>
2020-08-24 16:10:56 +0300

    Add missed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CardModule } from './card/card.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
